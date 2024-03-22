48dd2d49982d2e06e888ab513963b10b8d80dff3
Hector Parra <phector040@gmail.com>
2024-03-22 17:08:14 -0400

    Created Post component

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react';
+import Post from './Post.jsx'
+
+
+function App(){
+
+return(
+
+  <div>
+
+    <h1>Fakebook!</h1>
+
+      {/* CreatePostForm */}
+
+      {/* Feed */}
+
+      {/* Post */}
+
+  </div>
+
+  )
+
+}
+
+export default App
+
---
+++ src/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Post(){
+    return(
+        <div>
+            <h3>Test Post</h3>
+            <p>This is a test post we'll edit later!</p>
+            <p>Likes: 30</p>
+            <p>Comments:</p>
+
+        </div>
+
+
+
+    )
+}
+
+export default Post
